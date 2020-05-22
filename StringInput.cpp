c9d86ae6a23ed1eb5c12feda13c89f92bc4499c3
Andreas Odin Kammerloher <kammerloher.andreas@gmail.com>
2020-05-22 13:41:43 +0200

    Use SDL2 text input

--- StringInput.cpp
+++ StringInput.cpp
@@ -20,48 +20,34 @@ StringInput::~StringInput() {}
 void StringInput::handle_input(SDL_Event event) {
     SDL_Color textColor = {255, 255, 255};
 
-    // If a key was pressed
+    std::string temp = str;
+
+    //Special key input
     if (event.type == SDL_KEYDOWN) {
-        // Keep a copy of the current version of the string
-        std::string temp = str;
-
-        // If the string less than maximum size
-        if (str.length() <= 16) {
-            // If the key is a space
-            if (event.key.keysym.unicode == (Uint16)' ' || event.key.keysym.unicode == (Uint16)'.') {
-                // Append the character
-                str += (char)event.key.keysym.unicode;
-            }
-
-            // If the key is a number
-            else if ((event.key.keysym.unicode >= (Uint16)'0') && (event.key.keysym.unicode <= (Uint16)'9')) {
-                // Append the character
-                str += (char)event.key.keysym.unicode;
-            }
-
-            // If the key is a uppercase letter
-            else if ((event.key.keysym.unicode >= (Uint16)'A') && (event.key.keysym.unicode <= (Uint16)'Z')) {
-                // Append the character
-                str += (char)event.key.keysym.unicode;
-            }
-
-            // If the key is a lowercase letter
-            else if ((event.key.keysym.unicode >= (Uint16)'a') && (event.key.keysym.unicode <= (Uint16)'z')) {
-                // Append the character
-                str += (char)event.key.keysym.unicode;
-                // std::cout << "String should be: " << str;
-            }
+        //Handle backspace
+        if (event.key.keysym.sym == SDLK_BACKSPACE && str.length() > 0) {
+            //lop off character
+            str.pop_back();
         }
-
-        // If backspace was pressed and the string isn't blank
-        if ((event.key.keysym.sym == SDLK_BACKSPACE) && (str.length() != 0)) {
-            // Remove a character from the end
-            str.erase(str.length() - 1);
+        //Handle copy
+        else if (event.key.keysym.sym == SDLK_c && SDL_GetModState() & KMOD_CTRL) {
+            SDL_SetClipboardText(str.c_str());
         }
-
-        // If the string was changed
-        if (str != temp) {
-            rerenderpls = true;
+        //Handle paste
+        else if (event.key.keysym.sym == SDLK_v && SDL_GetModState() & KMOD_CTRL) {
+            str = SDL_GetClipboardText();
         }
+    }
+    //Special text input event
+    else if (event.type == SDL_TEXTINPUT) {
+        //Not copy or pasting
+        if (!(SDL_GetModState() & KMOD_CTRL && (event.text.text[0] == 'c' || event.text.text[0] == 'C' || event.text.text[0] == 'v' || event.text.text[0] == 'V'))) {
+            str += event.text.text;
+        }
+    }
+
+    // If the string was changed
+    if (str != temp) {
+        rerenderpls = true;
     }
 }
