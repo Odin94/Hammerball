e1e7f2dfd7653bea560472b1962663985d197fc9
Andreas Odin Kammerloher <kammerloher.andreas@gmail.com>
2020-05-22 14:07:12 +0200

    Fix function signature

--- Utility.h
+++ Utility.h
@@ -4,11 +4,9 @@
 #include <SDL_mixer.h>
 
 std::string toString(int number);
-SDL_Surface *load_image(std::string filename);
-void apply_surface(float drawscale, int x, int y, SDL_Surface *source,
-                   SDL_Surface *destination, SDL_Rect *clip = NULL);
-void apply_surface(float drawscale, int x, int y, SDL_Surface *source,
-                   SDL_Surface *destination, int xr, int yr, int w, int h);
+SDL_Surface *load_texture(std::string path, SDL_Renderer *renderer);
+void apply_surface(float drawscale, int x, int y, SDL_Surface *source, SDL_Surface *destination, SDL_Rect *clip = NULL);
+void apply_surface(float drawscale, int x, int y, SDL_Surface *source, SDL_Surface *destination, int xr, int yr, int w, int h);
 std::string toStr(int number);
 std::string toDottedIP(Uint32 rawIP);
 bool checkCollision(int x, int y, int w, int h, int x2, int y2, int w2, int h2);
